c38b43b84ceebb880cbb0a3bea3c583fa353a60d
Eric Hotaling <eric.hotaling@outlook.com>
2025-03-05 21:07:10 -0500

    Refactored RealTimeTestProcess to force it to go over its alloted time, allowing testing of process demotion.

--- src/RealTimeTestProcess.java
+++ src/RealTimeTestProcess.java
@@ -1,14 +1,30 @@
 public class RealTimeTestProcess extends UserlandProcess {
     @Override
     public void main() throws InterruptedException {
-        // int pid = OS.GetPID();
+
+        // TODO this breaks the program
+        int pid = OS.GetPID();
         // System.out.println("RealTimeTestProcess started with PID: " + pid);
         System.out.println("Realtimetest process started for the first time.");
         cooperate();
+        double dummy = 0;
         while (true) {
-            for (double i = 0; i < 100_000; i++) {
-                double dummy = Math.sin(i);
+            for (double i = 0; i < 100_000_000; i++) {
+                 dummy = Math.sin(i);
+                 for (double j = 0; j < 100_000_000; j++) {
+                     dummy = Math.cos(i+j);
+                     for (double k = 0; k < 100_000_000; k++) {
+                         dummy = Math.cos(i+k) * dummy;
+                         for (double l = 0; l < 100_000_000; l++) {
+                             dummy = Math.cos(i+l) * dummy;
+                             for (double n = 0; n < 100_000_000; n++) {
+                                 dummy = Math.cos(i+n) * dummy;
+                             }
+                         }
+                     }
+                 }
             }
+            System.out.println("Realtimetest process cooperating.");
             cooperate();
         }
     }
--- src/SleepTestProcess.java
+++ src/SleepTestProcess.java
@@ -1,7 +1,9 @@
+
 public class SleepTestProcess extends UserlandProcess {
     @Override
     public void main() throws InterruptedException {
         while (true) {
+            // TODO OS.GetPID() breaks the program
             // System.out.println("SleepTestProcess (PID: " + OS.GetPID() + ") running at real-time priority.");
             System.out.println("Sleeptest process is running.");
             OS.Sleep(100); // Sleep for 100 milliseconds before hitting timeout
